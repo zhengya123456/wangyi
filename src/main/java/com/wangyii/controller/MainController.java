e10930c6a856ddc83f770a4c770768c447fe1de2
xiaoQuanTxue <673038225@qq.com>
2020-06-10 16:54:13 +0800

    example

--- src/main/java/com/wangyii/controller/MainController.java
+++ src/main/java/com/wangyii/controller/MainController.java
@@ -2,11 +2,19 @@ package com.wangyii.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
     @RequestMapping("/hello")
     public String hello(){
-        return "hello";
+        return "/hello.jsp";
+    }
+    @RequestMapping("/new")
+    public ModelAndView newp(){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("new");
+        modelAndView.addObject("num",8);
+        return modelAndView;
     }
 }
